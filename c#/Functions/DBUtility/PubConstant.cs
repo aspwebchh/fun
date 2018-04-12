388781f2f1fc03eb5fde2df5ad811a6829edf3a8
CHENHONGHONG\宏鸿 <aspwebchh@hotmail.com>
2018-04-12 12:10:34 +0800

    c# sqlserver访问类库

---
+++ c#/Functions/DBUtility/PubConstant.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Configuration;
+namespace DBUtility.SqlServer
+{
+    
+    public class PubConstant
+    {        
+        /// <summary>
+        /// 获取连接字符串
+        /// </summary>
+        public static string ConnectionString
+        {           
+            get 
+            {     
+                string _connectionString = "Max Pool Size = 512;Server=192.168.110.227;DataBase=dbname;uid=sa;pwd=123456;";
+                string ConStringEncrypt = ConfigurationManager.AppSettings["ConStringEncrypt"];
+                if (ConStringEncrypt == "true")
+                {
+                    _connectionString = DESEncrypt.Decrypt(_connectionString);
+                }
+                return _connectionString; 
+            }
+        }
+
+        /// <summary>
+        /// 得到web.config里配置项的数据库连接字符串。
+        /// </summary>
+        /// <param name="configName"></param>
+        /// <returns></returns>
+        public static string GetConnectionString(string configName)
+        {
+            string connectionString = ConfigurationManager.AppSettings[configName];
+            string ConStringEncrypt = ConfigurationManager.AppSettings["ConStringEncrypt"];
+            if (ConStringEncrypt == "true")
+            {
+                connectionString = DESEncrypt.Decrypt(connectionString);
+            }
+            return connectionString;
+        }
+
+
+    }
+}
